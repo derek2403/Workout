fa945d0b87ae0af6583d8edcfafa927832edbf3e
Liew Qi Jian <liewqj-wc21@student.tarc.edu.my>
2024-06-11 14:56:38 +0800

    #4

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
-const express = require('express'); 
 require('dotenv').config();
+
+const express = require('express'); 
 const workoutRoutes = require('./routes/workout');
+const mongoose = require('mongoose');
 
 const app = express();
 app.use(express.json());
@@ -8,11 +10,16 @@ app.use(express.json());
 app.use((req,res,next) => {
     console.log(req.path, req.method);
     next(); 
-}); 
+});
 
 app.use('/api/workouts',workoutRoutes);
 
-
-app.listen(process.env.PORT, () => {
-  console.log('Server is running on port', process.env.PORT);
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log('Server is connected to database running on port', process.env.PORT);
+      });      
+})
+.catch((err) => {
+    console.log(err);
 });
